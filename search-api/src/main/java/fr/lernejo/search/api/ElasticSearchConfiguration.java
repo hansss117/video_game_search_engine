5ea98b0dc74940e24baa8ba51be19b4a1a42b098
hansss117 <hany.shamloul@epita.fr>
2023-01-10 14:11:24 +0100

    Partie 3 File Injector

--- search-api/src/main/java/fr/lernejo/search/api/ElasticSearchConfiguration.java
+++ search-api/src/main/java/fr/lernejo/search/api/ElasticSearchConfiguration.java
@@ -5,10 +5,10 @@ import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -37,12 +37,10 @@ public class ElasticSearchConfiguration {
 
         CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
         credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
-        RestClientBuilder builder = RestClient.builder(new HttpHost(host, port)).setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
-            @Override
-            public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpAsyncClientBuilder) {
-                return httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
-            }
-        });
+
+        RestClientBuilder builder = RestClient.builder(new HttpHost(host, port))
+            .setHttpClientConfigCallback(httpAsyncClientBuilder -> httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
+
         RestHighLevelClient client = new RestHighLevelClient(builder);
         return client;
     }
